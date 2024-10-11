c37c9ee37bb07d7d30403ad6e3f9315993588ba4
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-10-11 23:48:10 +0300

    1.1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-// import { useState, useEffect} from "react";
+// import { useState, useEffect } from "react";
 
 // import ContactForm from "./ContactForm/ContactForm";
 // import ContactList from "./ContactList/ContactList";
@@ -7,6 +7,7 @@
 // export default function App() {
 //   const [contacts, setContacts] = useState(() => {
 //     const initialContacts = localStorage.getItem("contacts");
+
 //     return initialContacts
 //       ? JSON.parse(initialContacts)
 //       : [
@@ -17,9 +18,9 @@
 //         ];
 //   });
 
-//     useEffect(() => {
-//       localStorage.setItem("contacts", JSON.stringify(contacts));
-//     });
+//   useEffect(() => {
+//     localStorage.setItem("contacts", JSON.stringify(contacts));
+//   });
 
 //   const [filter, setFilter] = useState("");
 
@@ -51,17 +52,56 @@
 //   );
 // }
 
-import { Layout } from "../Layout/Layout";
-import { AppBar } from "../AppBar/AppBar";
-import { TaskForm } from "../TaskForm/TaskForm";
-import { TaskList } from "../TaskList/TaskList";
+import { useState, useEffect } from "react";
+
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
 
 export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    const initialContacts = localStorage.getItem("contacts");
+
+    return initialContacts
+      ? JSON.parse(initialContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  });
+
+  const [filter, setFilter] = useState("");
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contactId !== contact.id);
+    });
+  };
+
+  const visibleContacts = contacts.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+  );
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
   return (
-    <Layout>
-      <AppBar />
-      <TaskForm />
-      <TaskList />
-    </Layout>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+    </div>
   );
 }
