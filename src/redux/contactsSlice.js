a6a6edbc4234161e6bda09a13f105fb08d5162f2
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-10-12 14:50:17 +0300

    ??

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,20 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const contactsSlice = createSlice({
+//   name: "contacts",
+//   initialState: {
+//     items: [],
+//   },
+//   reducers: {
+//     addContact: (state, action) => {
+//       state.push(action.payload);
+//     },
+//     deleteContact: (state, action) => {
+//       return state.filter((contact) => contact.id === action.payload.id);
+//     },
+//   },
+// });
+
+// export const { addContact, removeContact } = contactsSlice.actions;
+// export const selectContacts = (state) => state.contacts.items;
+// export default contactsSlice.reducer;
