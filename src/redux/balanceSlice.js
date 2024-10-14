d8e0ac4a65399f3d6ec7965c141bc4c349a4acad
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-10-14 16:28:24 +0300

    2.3

--- src/redux/balanceSlice.js
+++ src/redux/balanceSlice.js
@@ -1,24 +1,44 @@
-import { createAction } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-export const deposit = createAction("balance/deposit");
+// export const deposit = createAction("balance/deposit");
 
-export const withdraw = createAction("balance/withdraw");
+// export const withdraw = createAction("balance/withdraw");
 
-export default function balanceReducer(state = { value: 500 }, action) {
-  switch (action.type) {
-    case "balance/deposit":
-      return {
-        ...state,
-        value: state.value + action.payload,
-      };
+// export default function balanceReducer(state = { value: 500 }, action) {
+//   switch (action.type) {
+//     case "balance/deposit":
+//       return {
+//         ...state,
+//         value: state.value + action.payload,
+//       };
 
-    case "balance/withdraw":
-      return {
-        ...state,
-        value: state.value - action.payload,
-      };
+//     case "balance/withdraw":
+//       return {
+//         ...state,
+//         value: state.value - action.payload,
+//       };
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "balance",
+  initialState: {
+    value: 500,
+  },
+  reducers: {
+    deposit: (state, action) => {
+      state.value += action.payload;
+    },
+    withdraw: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
+
+export const { deposit, withdraw } = slice.actions;
+
+export default slice.reducer;
--- src/redux/localeSlice.js
+++ src/redux/localeSlice.js
@@ -1,16 +1,36 @@
-import { createAction } from "@reduxjs/toolkit";
+// import { createAction } from "@reduxjs/toolkit";
 
-export const changeLang = createAction("locale/changeLang");
+// export const changeLang = createAction("locale/changeLang");
 
-export default function localeReducer(state = { lang: "uk" }, action) {
-  switch (action.type) {
-    case "locale/changelang":
-      return {
-        ...state,
-        lang: action.payload,
-      };
+// export default function localeReducer(state = { lang: "uk" }, action) {
+//   switch (action.type) {
+//     case "locale/changelang":
+//       return {
+//         ...state,
+//         lang: action.payload,
+//       };
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const selectLang = (state) => state.locale.lang;
+
+const slice = createSlice({
+  name: "locale",
+  initialState: {
+    lang: "uk",
+  },
+  redusers: {
+    changeLang: (state, action) => {
+      state.lang = action.payload;
+    },
+  },
+});
+
+export const { changeLang } = slice.actions;
+
+export default slice.reducer;
