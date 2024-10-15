01c720295c192ba846d7b995b10760cdf8d7f18c
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-10-15 17:55:53 +0300

    2.4

--- src/components/App/ContactList/ContactList.jsx
+++ src/components/App/ContactList/ContactList.jsx
@@ -1,12 +1,32 @@
-import css from "./ContactList.module.css"
+// import css from "./ContactList.module.css";
+// import Contact from "../ContactList/Contact/Contact";
+
+// export default function ContactList({ contacts, onDelete }) {
+//   return (
+//     <ul className={css.ul}>
+//       {contacts.map((contact) => (
+//         <li key={contact.id}>
+//           <Contact data={contact} onDelete={onDelete} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+import css from "./ContactList.module.css";
 import Contact from "../ContactList/Contact/Contact";
 
-export default function ContactList({ contacts, onDelete }) {
+import { userData } from "../../../redux/contactsSlice";
+import { useSelector } from "react-redux";
+
+export default function ContactList({ onDelete }) {
+  const userdata = useSelector(userData);
+
   return (
     <ul className={css.ul}>
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          <Contact data={contact} onDelete={onDelete} />
+      {userdata.map((user) => (
+        <li key={user.id}>
+          <Contact data={user} onDelete={onDelete} />
         </li>
       ))}
     </ul>
